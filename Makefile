3a2ca69233834bb66a2b54930c46dc790961bf7a
servostar <72654954+Servostar@users.noreply.github.com>
2024-12-03 10:11:51 +0100

    ci: check style (#1)
    build: add check and format to Makefile
    build: add typstyle format script and nix shell
    style: format main.typ
    ci: add checks action
    build: update template to v0.6.3
    ci: rename artifact

--- Makefile
+++ Makefile
@@ -3,11 +3,17 @@ include .env
 
 build:
 	git submodule update --init --recursive
-	typst compile src/main.typ \
+	typst compile "${TYPST_MAIN}" \
     	--root . \
     	--font-path template/fonts \
     	--pdf-standard a-2b \
     	"${TYPST_FILE_PATH}.${TYPST_FILE_TYPE}"
+
+check:
+	scripts/typstyle.sh --check "${TYPST_MAIN}"
+
+format:
+	scripts/typstyle.sh --format "${TYPST_MAIN}"
 
 clean:
 	rm "${TYPST_FILE_PATH}.${TYPST_FILE_TYPE}"
---
+++ scripts/typstyle.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Check and apply format using Typstyle
+# =============================================================================
+# Run with --format on an input file to recursively check the format of this
+# file and all of its dependencies.
+# Run with --check on an input file to recursively format the content of this
+# file and all of its dependencies.
+#
+# Copied from: https://git.montehaselino.de/DHBW/dhbw-abb-typst-template/src/branch/main/run-fmt.sh
+#
+
+function format() {
+    # format file
+    # typstyle --format $1
+
+    if [ -z "${1}" ]; then
+        return
+    fi
+
+    local wd=$(dirname $(realpath "$1"))
+
+    echo "processing file $1..."
+    typstyle "${2}" "${1}" > /dev/null
+    if [ $? -eq 1 ]; then
+        echo "failed format validation: ${1}"
+        exit 1
+    fi
+
+    local imports=$(rg "#import \"([a-z0-9/\-.]+\.typ)\"" -Nor '$1' "${1}")
+
+    # format all imported files
+    while IFS= read -r line; do
+        if [ -z "${line}" ]; then
+            continue
+        fi
+        format "${wd}/${line}" "${2}"
+    done <<< "$imports"
+
+    local includes=$(rg "#include \"([a-z0-9/\-.]+\.typ)\"" -Nor '$1' "${1}")
+
+    # format all included files
+    while IFS= read -r line; do
+        if [ -z "${line}" ]; then
+            continue
+        fi
+        format "${wd}/${line}" "${2}"
+    done <<< "${includes}"
+}
+
+case $1 in
+  "--format")
+    format "${2}" "--inplace"
+    ;;
+  "--check")
+    format "${2}" "--check"
+    ;;
+  *)
+    echo "unknown option: ${1}"
+    exit 1
+    ;;
+esac
