f252e96aceb5af4960f3c8f8dedf5a9fb702179a
servostar <sven.vogel123@web.de>
2024-12-07 16:28:45 +0100

    ops: use nix channel 24.11

--- shell.nix
+++ shell.nix
@@ -7,7 +7,7 @@
 #
 
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 
