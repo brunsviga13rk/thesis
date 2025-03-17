12149f3390cfa32baac4cd0524a22d2e3277a7df
servostar <sven.vogel123@web.de>
2025-03-17 23:02:09 +0100

    fix: update format script

--- scripts/typstyle.sh
+++ scripts/typstyle.sh
@@ -9,54 +9,55 @@
 #
 # Copied from: https://git.montehaselino.de/DHBW/dhbw-abb-typst-template/src/branch/main/run-fmt.sh
 #
+#!/bin/bash
 
 function format() {
     # format file
     # typstyle --format $1
 
-    if [ -z "${1}" ]; then
+    if [ -z "$1" ]; then
         return
     fi
 
     local wd=$(dirname $(realpath "$1"))
 
     echo "processing file $1..."
-    typstyle "${2}" "${1}" > /dev/null
+    typstyle "$2" "$1" > /dev/null
     if [ $? -eq 1 ]; then
-        echo "failed format validation: ${1}"
+        echo "failed format validation: $1"
         exit 1
     fi
 
-    local imports=$(rg "#import \"([a-z0-9/\-.]+\.typ)\"" -Nor '$1' "${1}")
-
-    # format all imported files
-    while IFS= read -r line; do
-        if [ -z "${line}" ]; then
-            continue
-        fi
-        format "${wd}/${line}" "${2}"
-    done <<< "$imports"
-
-    local includes=$(rg "#include \"([a-z0-9/\-.]+\.typ)\"" -Nor '$1' "${1}")
+    local imports=$(rg "#import \"([a-z0-9/\-.]+\.typ)\"" -Nor '$1' "$1")
 
     # format all included files
     while IFS= read -r line; do
-        if [ -z "${line}" ]; then
+        if [ -z "$line" ]; then
             continue
         fi
-        format "${wd}/${line}" "${2}"
-    done <<< "${includes}"
+        format "$wd/$line" "$2"
+    done <<< "$imports"
+
+    local includes=$(rg "#import \"([a-z0-9/\-.]+\.typ)\"" -Nor '$1' "$1")
+
+    # format all included files
+    while IFS= read -r line; do
+        if [ -z "$line" ]; then
+            continue
+        fi
+        format "$wd/$line" "$2"
+    done <<< "$includes"
 }
 
 case $1 in
   "--format")
-    format "${2}" "--inplace"
+    format "$2" "--inplace"
     ;;
   "--check")
-    format "${2}" "--check"
+    format "$2" "--check"
     ;;
   *)
-    echo "unknown option: ${1}"
+    echo "unknown option: $1"
     exit 1
     ;;
 esac
